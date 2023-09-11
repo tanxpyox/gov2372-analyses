7b0c997fd2f018872e06becc33bacceef0b269ad
亶父 <tanxpyox@gmail.com>
2023-09-11 09:35:54 -0400

    update

--- week1.R
+++ week1.R
@@ -2,6 +2,7 @@ library(tidyverse)
 library(readr)
 library(readxl)
 library(magrittr)
+library(interplot)
 
 # Load dataset
 df <- read_excel("data/1_Sep13/Tybur_data.xlsx")
@@ -26,4 +27,29 @@ cor(df$DS_total, df$DS, use="complete.obs")
 cor(df$trad_total, df$Traditionalim, use="complete.obs")
 cor(df$sd_total, df$SDO, use="complete.obs")
 
+df %<>% mutate(
+  pdeaths_per1k = WHO2012infectiousparasiticdiseasedeathsper1Kpopn,
+  parasite = Parasite_PCA,
+  trad = Traditionalim,
+  sd = SDO,
+  ds = DS
+)
 
+country <- df %>% group_by(nation) %>% summarise(
+  nation_name = nation_name[1],
+  nation_abbrev = nation_abbrev[1],
+  ds_average = mean(DS),
+  trad_average = mean(Traditionalim),
+  sdo_average = mean(SDO),
+  pdeaths_per1k = pdeaths_per1k[1],
+  parasite = Parasite_PCA[1],
+  zoonotic = ZoonoticParasitePrevalence[1],
+  nonzoonotic = NonzoonoticParasitePrevalence[1]
+)
+
+r1 <- lm(ds ~ pdeaths_per1k * parasite, data = df) %>% summary
+# r2 <- lm(trad ~ pdeaths_per1k * parasite, data = df) %>% summary
+# r3 <- lm(trad ~ pdeaths_per1k * ds * parasite, data = df) %>% summary
+
+# plot_model(r, type = "pred", terms = c("pdeaths_per1k", "parasite"))
+interplot(r1, var1 = "pdeaths_per1k", var2 = "pdeaths_per1k")
